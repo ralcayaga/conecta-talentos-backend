3a95a8f966a3106262f793158a632129fe94f8ad
ralcayag <ralcayaga74@gmail.com>
2024-07-18 20:14:34 -0400

    Módulo de estudiantes

--- src/estudiantes/estudiantes.controller.ts
+++ src/estudiantes/estudiantes.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param} from '@nestjs/common';
+import { EstudianteService } from './estudiantes.service';
 
-@Controller('estudiantes')
-export class EstudiantesController {}
+
+@Controller('estudiante')
+export class EstudiantesController {
+  constructor(private readonly estudianteService: EstudianteService) {}
+
+  @Post()
+  crear(@Body() estudiante: any): any {
+    return this.estudianteService.crear(estudiante);
+  }
+
+  @Get()
+  obtenerTodos(): any[] {
+    return this.estudianteService.obtenerTodos();
+  }
+
+  @Get(':id')
+  obtenerPorId(@Param('id') id: string): any {
+    return this.estudianteService.obtenerPorId(id);
+  }
+}
--- src/estudiantes/estudiantes.service.ts
+++ src/estudiantes/estudiantes.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EstudiantesService {}
+export class EstudianteService {
+  private estudiantes: any[] = [];
+
+  crear(estudiante: any): any {
+    const nuevoEstudiante = {
+      id: Date.now().toString(),
+      ...estudiante,
+    };
+    this.estudiantes.push(nuevoEstudiante);
+    return nuevoEstudiante;
+  }
+
+  obtenerTodos(): any[] {
+    return this.estudiantes;
+  }
+
+  obtenerPorId(id: string): any {
+    return this.estudiantes.find(estudiante => estudiante.id === id);
+  }
+}
